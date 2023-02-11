c3497aafa43b3f171b9fa29a4d0731639a06302e
Snigdha <snigdha.sahu1999@gmail.com>
2023-02-12 01:20:49 +0530

    Changes Updated

--- src/com/snigdha/springdemo/HelloSpringApp.java
+++ src/com/snigdha/springdemo/HelloSpringApp.java
@@ -28,10 +28,21 @@ public class HelloSpringApp {
 		System.out.println(myTrackCoach.getDailyFortune());*/
 		
 		//Practice Activity #2
-		Coach theBadmintonCoach = context.getBean("myCoach2",Coach.class);
+		/*Coach theBadmintonCoach = context.getBean("myCoach2",Coach.class);
 		System.out.println(theBadmintonCoach.getDailyWorkout());
 		System.out.println(theBadmintonCoach.getDailyFortune());
-		System.out.println(theBadmintonCoach.getDailyRandomFortune());
+		System.out.println(theBadmintonCoach.getDailyRandomFortune());*/
+		
+		////Practice Activity #3
+		Coach theBadmintonCoach = context.getBean("myCoach2",Coach.class);
+		Coach theBadCoach = context.getBean("myCoach2",Coach.class);
+		boolean result = (theBadmintonCoach == theBadCoach);
+		
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for theBadmintonCoach: "+ theBadmintonCoach);
+		
+		System.out.println("\nMemory location for theBadCoach: "+ theBadCoach + "\n"  );
 		// close the context
 		context.close();
 	}
